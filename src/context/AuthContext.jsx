5d95b76a3b9c2d9e9cea095f45440a6d4a86488c
enrique cruz <luis.ecruzt@outlook.com>
2026-09-16 00:56:37 -0600

    supabase config

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,21 @@
+import {
+  Children,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { supabase, LinksArray } from "../index";
+
+const AuthContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+  const [user, setUser] = useState([]);
+  useEffect(() => {
+    const { data: authListener } = supabase.auth.onAuthStateChange(
+      (event, session) => {
+        console.log(event, session);
+      },
+    );
+  }, []);
+};
